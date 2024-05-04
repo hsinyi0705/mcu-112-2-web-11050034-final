02a20037feace177394c512c8dd23dc85263f6d4
HsinYi <11050034@me.mcu.edu.tw>
2024-05-04 18:57:40 +0800

    feat(router): 切換產品清單與購物車頁面實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  { path: 'product-list', component: ProductListComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+];
