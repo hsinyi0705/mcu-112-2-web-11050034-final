97fcbd61d93d2bf64897b52e2aba952378a7013b
HsinYi <11050034@me.mcu.edu.tw>
2024-05-06 22:21:56 +0800

    feat(product-card): 產品卡片樣式實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SearchComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchComponent, ProductCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
