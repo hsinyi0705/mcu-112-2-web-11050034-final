a75ac218189afe811a4ac447678fb5abbf2842b2
HsinYi <11050034@me.mcu.edu.tw>
2024-05-06 21:42:35 +0800

    feat(search): 產品查詢框樣式實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
