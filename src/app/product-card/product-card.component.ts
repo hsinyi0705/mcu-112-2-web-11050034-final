1413e68f6d4bf2fe1a5f8de57ddd6b059b77079f
HsinYi <11050034@me.mcu.edu.tw>
2024-06-14 12:11:11 +0800

    feat(product-card): 利用文字插值顯示產品名稱、作者、公司

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -5,8 +5,10 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.css'
+  styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
-
+  productName = 'A 產品';
+  authors = '作者、作者B、作者C';
+  company = '博碩文化';
 }
