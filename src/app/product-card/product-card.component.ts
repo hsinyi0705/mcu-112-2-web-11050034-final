38c1b1042d0d7875e780611a12b4e7c9f1ca62c7
HsinYi <11050034@me.mcu.edu.tw>
2024-06-14 12:16:13 +0800

    feat(product-card): 利用屬性繫結實作圖片位址繫結

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 })
 export class ProductCardComponent {
   productName = 'A 產品';
-  authors = '作者、作者B、作者C';
+  authors = '作者A、作者B、作者C';
   company = '博碩文化';
+  imgUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
 }
