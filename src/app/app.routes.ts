576e30c4cbd1e2be791e971e0200e897845d6f3b
HsinYi <11050034@me.mcu.edu.tw>
2024-06-17 18:42:38 +0800

    fix(route): 修正產品清單頁面路由

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'products' },
-  { path: 'product-list', component: ProductListComponent },
+  { path: 'products', component: ProductPageComponent },
   { path: 'shopping-cart', component: ShoppingCartComponent },
 ];
