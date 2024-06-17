cf399be2eb7b96768969a5d9955806959e8ffdac
HsinYi <11050034@me.mcu.edu.tw>
2024-06-17 23:32:16 +0800

    feat(pagination): 新增分頁元件

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,18 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../model/product';
+import { PaginationComponent } from '../pagination/pagination.component';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [SearchComponent, ProductCardComponent],
+  imports: [SearchComponent, ProductCardComponent, PaginationComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
   @Input({ required: true })
   products!: Product[];
+
+  pageIndex = 1;
 
   @Output()
   view = new EventEmitter<Product>();
