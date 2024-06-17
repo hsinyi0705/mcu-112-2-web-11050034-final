78dd686cef4474158e8270b6c18417b4112e9a56
HsinYi <11050034@me.mcu.edu.tw>
2024-06-17 18:47:50 +0800

    feat(product): 依產品清單顯示實作

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { Product } from '../model/product';
+import { ProductListComponent } from '../product-list/product-list.component';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [],
+  imports: [ProductListComponent],
   templateUrl: './product-page.component.html',
-  styleUrl: './product-page.component.css'
+  styleUrl: './product-page.component.css',
 })
 export class ProductPageComponent {
-
+  products = [
+    new Product({
+      id: 1,
+      productName: 'A 產品',
+      authors: '作者A、作者B、作者C',
+      company: '博碩文化',
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
+      price: 1580,
+    }),
+    new Product({
+      id: 2,
+      productName: 'B 產品',
+      authors: '作者A、作者B、作者C',
+      company: '博碩文化',
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
+      price: 1580,
+    }),
+    new Product({
+      id: 3,
+      productName: 'C 產品',
+      authors: '作者A、作者B、作者C',
+      company: '博碩文化',
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
+      price: 1580,
+    }),
+    new Product({
+      id: 4,
+      productName: 'D 產品',
+      authors: '作者A、作者B、作者C',
+      company: '博碩文化',
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
+      price: 1580,
+    }),
+    new Product({
+      id: 5,
+      productName: 'E 產品',
+      authors: '作者A、作者B、作者C',
+      company: '博碩文化',
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
+      price: 1580,
+    }),
+  ];
 }
