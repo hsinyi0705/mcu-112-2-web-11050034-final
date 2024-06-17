09320fcbbc3bf8c2064cb51212a35e4acebcb293
HsinYi <11050034@me.mcu.edu.tw>
2024-06-17 19:47:24 +0800

    feat(service): 從服務取得產品資料

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../model/product';
 import { ProductListComponent } from '../product-list/product-list.component';
+import { ProductService } from '../services/product.services';
 
 @Component({
   selector: 'app-product-page',
@@ -10,51 +11,17 @@ import { ProductListComponent } from '../product-list/product-list.component';
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css',
 })
-export class ProductPageComponent {
-  products = [
-    new Product({
-      id: 1,
-      productName: 'A 產品',
-      authors: ['作者A、作者B、作者C'],
-      company: '博碩文化',
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
-      price: 1580,
-    }),
-    new Product({
-      id: 2,
-      productName: 'B 產品',
-      authors: ['作者A、作者B、作者C'],
-      company: '博碩文化',
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
-      price: 1580,
-    }),
-    new Product({
-      id: 3,
-      productName: 'C 產品',
-      authors: ['作者A、作者B、作者C'],
-      company: '博碩文化',
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
-      price: 1580,
-    }),
-    new Product({
-      id: 4,
-      productName: 'D 產品',
-      authors: ['作者A、作者B、作者C'],
-      company: '博碩文化',
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
-      price: 1580,
-    }),
-    new Product({
-      id: 5,
-      productName: 'E 產品',
-      authors: ['作者A、作者B、作者C'],
-      company: '博碩文化',
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
-      price: 1580,
-    }),
-  ];
-
+export class ProductPageComponent implements OnInit {
   router = inject(Router);
+
+  private productService!: ProductService;
+
+  products!: Product[];
+
+  ngOnInit(): void {
+    this.productService = new ProductService();
+    this.products = this.productService.getList();
+  }
 
   onView(product: Product): void {
     this.router.navigate(['product', product.id]);
---
+++ src/app/services/product.services.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../model/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private _data = [
+    new Product({
+      id: 1,
+      productName: 'A 產品',
+      authors: ['作者A、作者B、作者C'],
+      company: '博碩文化',
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
+      price: 1580,
+    }),
+    new Product({
+      id: 2,
+      productName: 'B 產品',
+      authors: ['作者A、作者B、作者C'],
+      company: '博碩文化',
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
+      price: 1580,
+    }),
+    new Product({
+      id: 3,
+      productName: 'C 產品',
+      authors: ['作者A、作者B、作者C'],
+      company: '博碩文化',
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
+      price: 1580,
+    }),
+    new Product({
+      id: 4,
+      productName: 'D 產品',
+      authors: ['作者A、作者B、作者C'],
+      company: '博碩文化',
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
+      price: 1580,
+    }),
+    new Product({
+      id: 5,
+      productName: 'E 產品',
+      authors: ['作者A、作者B、作者C'],
+      company: '博碩文化',
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
+      price: 1580,
+    }),
+  ];
+
+  getList(): Product[] {
+    return this._data;
+  }
+}
