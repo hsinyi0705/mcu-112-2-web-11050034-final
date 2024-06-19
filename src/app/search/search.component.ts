0ac2f2512abda86f9070525234e0bf0b8dee9858
HsinYi <11050034@me.mcu.edu.tw>
2024-06-19 11:30:53 +0800

    feat(product): 查詢列表單設定

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,14 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
 export class SearchComponent {
-
+  protected readonly formControl = new FormControl<string | undefined>(undefined);
 }
