4108f74d0958689360b2260c5ba3953e74388b70
HsinYi <11050034@me.mcu.edu.tw>
2024-06-19 13:02:05 +0800

    feat(product): 產品資料查詢實作

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,8 +1,9 @@
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Subject, combineLatest, startWith, switchMap, tap } from 'rxjs';
 
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Product } from '../model/product';
 import { ProductListComponent } from '../product-list/product-list.component';
 import { ProductService } from '../services/product.service';
@@ -10,7 +11,7 @@ import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [AsyncPipe, ProductListComponent],
+  imports: [AsyncPipe, ProductListComponent, JsonPipe, ReactiveFormsModule],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css',
 })
@@ -22,6 +23,8 @@ export class ProductPageComponent {
   private readonly refresh$ = new Subject<void>();
 
   protected pageSize = 5;
+
+  protected readonly formControl = new FormControl<string | undefined>(undefined, { nonNullable: true });
 
   private readonly condition$ = new BehaviorSubject<string | undefined>(undefined);
   get condition() {
