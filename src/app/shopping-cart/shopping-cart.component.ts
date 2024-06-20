655cbe614d896aab0fb52c34e56300be35a16c52
HsinYi <11050034@me.mcu.edu.tw>
2024-06-20 21:47:24 +0800

    feat(shopping-cart): 購物車表單驗證

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,6 +1,6 @@
 import { CurrencyPipe, JsonPipe } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IOrderDetailForm } from '../interface/order-detail-form.interface';
 import { IOrderForm } from '../interface/order-form.interface';
 import { Product } from '../model/product';
@@ -17,11 +17,23 @@ export class ShoppingCartComponent implements OnInit {
   readonly shoppingCartService = inject(ShoppingCartService);
 
   readonly form = new FormGroup<IOrderForm>({
-    name: new FormControl<string | null>(null),
-    address: new FormControl<string | null>(null),
-    telephone: new FormControl<string | null>(null),
+    name: new FormControl<string | null>(null, { validators: [Validators.required] }),
+    address: new FormControl<string | null>(null, { validators: [Validators.required] }),
+    telephone: new FormControl<string | null>(null, { validators: [Validators.required] }),
     details: new FormArray<FormGroup<IOrderDetailForm>>([]),
   });
+
+  get name(): FormControl<string | null> {
+    return this.form.get('name') as FormControl<string | null>;
+  }
+
+  get address(): FormControl<string | null> {
+    return this.form.get('address') as FormControl<string | null>;
+  }
+
+  get telephone(): FormControl<string | null> {
+    return this.form.get('telephone') as FormControl<string | null>;
+  }
 
   get details(): FormArray<FormGroup<IOrderDetailForm>> {
     return this.form.get('details') as FormArray<FormGroup<IOrderDetailForm>>;
@@ -42,5 +54,9 @@ export class ShoppingCartComponent implements OnInit {
 
       this.details.push(control);
     }
+  }
+
+  onSave(): void {
+    console.log('Save');
   }
 }
