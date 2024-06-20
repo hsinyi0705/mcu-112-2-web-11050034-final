9f3924cd530ab27e3230fb4226af8346c83e490c
HsinYi <11050034@me.mcu.edu.tw>
2024-06-21 01:43:33 +0800

    feat(product-detail): 實作折扣產品名稱樣式

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,5 +1,5 @@
-import { CurrencyPipe } from '@angular/common';
-import { Component, Input, OnInit, inject, numberAttribute } from '@angular/core';
+import { CommonModule, CurrencyPipe, JsonPipe, NgIf } from '@angular/common';
+import { Component, Input, OnInit, booleanAttribute, inject, numberAttribute } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from '../services/product.service';
 import { ShoppingCartService } from '../services/shopping-cart.service';
@@ -8,13 +8,16 @@ import { Product } from './../model/product';
 @Component({
   selector: 'app-product-detail-page',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, NgIf, CommonModule, JsonPipe],
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.css',
 })
 export class ProductDetailPageComponent implements OnInit {
   @Input({ transform: numberAttribute })
   id!: number;
+
+  @Input({ transform: booleanAttribute })
+  vendue!: boolean;
 
   product!: Product;
 
